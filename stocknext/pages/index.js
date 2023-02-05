d2a644621d69f2572604b889edb71989c3d81de5
dubeyaashish <72406443+dubeyaashish@users.noreply.github.com>
2023-02-05 18:22:59 +0700

    Init

--- stocknext/pages/index.js
+++ stocknext/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-           Aashish Dubey 
+            Aashish Dubey
           </p>
           <div>
             <a
