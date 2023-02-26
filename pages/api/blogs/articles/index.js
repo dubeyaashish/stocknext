2d0af4b2094c7064698885bec40b54acc5068232
dubeyaashish <72406443+dubeyaashish@users.noreply.github.com>
2023-02-26 20:30:33 +0700

    init

--- pages/api/blogs/articles/index.js
+++ pages/api/blogs/articles/index.js
@@ -1,6 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createConnection, model, models, Schema } from "mongoose";
+const connectionString = process.env.MONGODB_URI;
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  await createConnectio(connectionString);
+  console.log("req.method: ", req.method)
+
+  if (req.method === 'GET') {
+      const docs = await Article.find()
+      res.status(200).json(docs)
+  } else if (req.method === 'POST') {
+      console.log(typeof(req.body))
+      // res.status(200).json(req.body)
+      const doc = await Article.create(req.body)
+      res.status(201).json(doc)
+  } else {
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
   }
-  
+}
+
+
+
+const articleSchema = new Schema({
+  title: String,
+  content: String,
+});
+
+console.log("Mongoose Models", models)
+const Article = models?.article || model('article', articleSchema);
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,31 +1,38 @@
-import { connect, model, models, Schema } from "mongoose"
-const connectionString = 'mongodb+srv://dubey-aashish:dubey9373aashish@cluster0.ngkkrxd.mongodb.net/stock'
+import { connect, model, models, Schema } from "mongoose";
+const connectionString = process.env.MONGODB_URI;
 
 export default async function handler(req, res) {
-    await connect(connectionString);
-    console.log("req.method: ", req.method)
+  await connect(connectionString);
+  console.log("req.method: ", req.method)
 
-    if (req.method === 'GET') {
-        const docs = await Product.find()
-        res.status(200).json(docs)
-    } else if (req.method === 'POST') {
-        console.log(req.body)
-        // res.status(200).json(req.body)
-        const doc = await Product.create(req.body)
-        res.status(201).json(doc)
-    } else {
-        res.setHeader('Allow', ['GET', 'POST'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
-    }
+  if (req.method === 'GET') {
+      const docs = await Product.find()
+      res.status(200).json(docs)
+  } else if (req.method === 'POST') {
+      console.log(typeof(req.body))
+      // res.status(200).json(req.body)
+      const doc = await Product.create(req.body)
+      res.status(201).json(doc)
+  } else {
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
 }
 
 
 
 const productSchema = new Schema({
-    code: String,
-    name: String,
-    price: Number,
+  code: String,
+  name: String,
+  price: Number,
 });
 
-console.log("Product Models", models)
-const Product = models?.products || model('products', productSchema);
+console.log("Mongoose Models", models)
+const Product = models?.product || model('product', productSchema);
+
+
+
+
+
+
+
